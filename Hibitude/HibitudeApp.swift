48ef689a590d4529aa181e01601faa4d3735c242
yonko <nexiathon@gmail.com>
2024-03-18 17:31:11 +0100

    Initial Commit

---
+++ Hibitude/HibitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HibitudeApp.swift
+//  Hibitude
+//
+//  Created by Robin Cassard on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HibitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
